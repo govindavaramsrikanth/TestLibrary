c328c79cc87677120b6fd1f0a3c35d0cd4395b52
srikanth govindavaram <govindavaramsrikanth@gmail.com>
2022-02-10 18:42:28 +0530

    TestUtil

--- Trace/src/main/java/com/example/trace/TraceUtil.java
+++ Trace/src/main/java/com/example/trace/TraceUtil.java
@@ -2,9 +2,14 @@ package com.example.trace;
 
 import android.util.Log;
 
-public class Trace {
+public class TraceUtil {
 
     public static void d(String tag, String message) {
+        Log.d(tag, message);
+    }
+
+
+    public void e(String tag, String message) {
         Log.d(tag, message);
     }
 }
